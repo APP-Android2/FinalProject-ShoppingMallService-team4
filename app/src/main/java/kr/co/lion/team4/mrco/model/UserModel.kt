f47b44baaf0e1cae8683e952d810ca6efde02b66
wtoshm <wtoshm@gmail.com>
2024-04-16 17:58:17 +0900

    UserModel에 회원가입일 추가, 기본값 설정 필요

--- app/src/main/java/kr/co/lion/team4/mrco/model/UserModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/model/UserModel.kt
@@ -3,7 +3,9 @@ package kr.co.lion.team4.mrco.model
 // 기존 필수 항목 회원번호, 아이디, 비밀번호, 이름, 성별, 이메일, MBTI,
 // 개인정보 제공동의, 상품정보 수신동의, 회원상태에
 // 추가 선택정보로 휴대폰 번호, 주소, 상세주소, 환불계좌 은행,
-// 환불계좌 예금주, 환불계좌 계좌번호, 키, 체중, 앱 알림설정 추가
+// 환불계좌 예금주, 환불계좌 계좌번호, 키, 체중, 앱 알림설정, 가입일 추가
+
+// 회원 가입일 기본값 설정 필요 **************
 
 data class UserModel(var userIdx:Int, var userId:String,
                      var userPw:String, var userName:String, var userGender:Int,
@@ -14,7 +16,7 @@ data class UserModel(var userIdx:Int, var userId:String,
                      var userRefundBankAccountHolder: String = "",
                      var userRefundBankAccountNumber: String = "",
                      var userHeight: String = "", var userWeight: String = "",
-                     var userNotification: Boolean = false,){
+                     var userNotification: Boolean = false, var userJoinDate: String = "",){
 
     // 매개 변수가 없는 생성자
     // fireStore 를 사용할 때 데이터를 담을 클래스 타입을 지정하게 되면
@@ -22,7 +24,7 @@ data class UserModel(var userIdx:Int, var userId:String,
     constructor() : this(0, "", "", "", 0, "",
         "", false, false, 0,
         "","", "", "", "",
-        "", "", "", false)
+        "", "", "", false, "")
 }
 
 
